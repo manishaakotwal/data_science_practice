5987b19bf8a403814d4d648272c1f82a43cdc233
Manisha <manishakotwal502@gmail.com>
2024-07-28 21:39:37 +0530

    code

---
+++ s1.py
@@ -0,0 +1,38 @@
+# #strings methods
+# text = " hello world "
+# print(text.upper())
+# print(text.lower())
+# print(text.capitalize())
+# print(text.title())
+# print(text.strip())
+# print(text.replace("world", "there"))
+# print(text.split()) # splits string
+# words = ['hello', 'world']
+# print(" ".join(words)) #iterable with string as a sep.
+# print(text.find("world")) 
+# print(text.count("o"))
+# text= "hello world"
+# print(text.startswith("hello"))
+# text= "hello world"
+# print(text.endswith("world"))
+# text ="12345"
+# print(text.isdigit())
+# text="hello"
+# print(text.isalpha())
+# print(text.isalnum())
+
+#basic level
+# write a pro. to conacte two strings
+a= "hello"
+b= "world"
+c = a+b
+print(c)
+print(" ".join([a,b]))
+#Write a program to find the length of a string.
+
+a ="hello"
+print(len(a))
+cnt =0
+for i in a:
+   cnt += 1
+print(cnt)        
